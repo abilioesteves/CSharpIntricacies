b4d693ecf182ff3053ff94c455dc18e75777cc68
Abílio Esteves <abilioe@lacunasoftware.com>
2017-11-19 14:52:05 -0200

    added little codedom test

---
+++ AUnitTestProject/CreateAndUseTypes/Reflection.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUnitTestProject.CreateAndUseTypes {
+
+    [TestClass, TestCategory(nameof(CreateAndUseTypes))]
+    public class Reflection {
+
+        [TestMethod]
+        public void CodeDOMWithExpressions() {
+            BlockExpression blockExpr = Expression.Block(
+                Expression.Call(
+                    null,
+                    typeof(Console).GetMethod("Write", new Type[] { typeof(String) }),
+                    Expression.Constant("Hello")
+                ),
+                Expression.Call(
+                    null,
+                    typeof(Console).GetMethod("WriteLine", new Type[] { typeof(String) }),
+                    Expression.Constant("World!")
+                    )
+                );
+
+            var sb = new StringBuilder();
+            
+            Expression.Lambda<Action>(blockExpr).Compile()();
+        }
+    }
+}
