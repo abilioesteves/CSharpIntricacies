9397fe5987a571db76dd00d432bc8318f4c7e569
Abílio Esteves <abilioe@lacunasoftware.com>
2017-11-19 09:17:43 -0200

    Added custom compareby attribute

---
+++ AClassLibrary/Misc/CompareByAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AClassLibrary.Misc {
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CompareByAttribute : Attribute {
+
+        public CompareByAttribute(int order) {
+            this.order = order;
+        }
+
+        public bool descending { get; set; }
+
+        private int order { get; set; }
+    }
+}
